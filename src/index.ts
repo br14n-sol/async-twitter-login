c0dd692b82befb73210d6f1c016754da8ba64ef8
br14n-sol <60621046+br14n-sol@users.noreply.github.com>
2023-02-22 11:47:51 -0500

    style: fix code style

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-/*
-  Package: async-twitter-login
-  License: MIT
-  Author: Brian Fernandez
-*/
 import { OAuth } from 'oauth'
 import {
-  IConstructorOptions,
-  ICallbackOptions,
-  IRequestResponse,
-  ICallbackResponse
+  type IConstructorOptions,
+  type ICallbackOptions,
+  type IRequestResponse,
+  type ICallbackResponse
 } from './types'
 
 const REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
@@ -26,13 +21,13 @@ class AsyncTwitterLogin {
     consumerSecret,
     callbackUrl
   }: IConstructorOptions) {
-    if (!consumerKey && typeof consumerKey !== 'string') {
+    if (typeof consumerKey !== 'string') {
       throw new Error('AsyncTwitterLogin: `consumerKey` is required')
     }
-    if (!consumerSecret && typeof consumerSecret !== 'string') {
+    if (typeof consumerSecret !== 'string') {
       throw new Error('AsyncTwitterLogin: `consumerSecret` is required')
     }
-    if (!callbackUrl && typeof callbackUrl !== 'string') {
+    if (typeof callbackUrl !== 'string') {
       throw new Error('AsyncTwitterLogin: `callbackUrl` is required')
     }
 
@@ -50,44 +45,54 @@ class AsyncTwitterLogin {
     )
   }
 
-  request (): Promise<IRequestResponse> {
-    return new Promise((resolve, reject) => {
+  async request (): Promise<IRequestResponse> {
+    return await new Promise((resolve, reject) => {
       this.oauth.getOAuthRequestToken((error, token, tokenSecret, results) => {
-        if (error) return reject(error)
+        if (error != null) {
+          reject(error)
+          return
+        }
 
         const { oauth_callback_confirmed: callbackConfirmed } = results
         if (callbackConfirmed !== 'true') {
-          return reject(new Error('AsyncTwitterLogin: `oauth_callback_confirmed` is not `true` (Does your application have a callback url configured?)'))
+          reject(new Error('AsyncTwitterLogin: `oauth_callback_confirmed` is not `true` (Does your application have a callback url configured?)'))
+          return
         }
 
         const redirectUrl = `${AUTHENTICATE_URL}?oauth_token=${token}`
-        return resolve({ token, tokenSecret, redirectUrl })
+        resolve({ token, tokenSecret, redirectUrl })
       })
     })
   }
 
-  callback ({
+  async callback ({
     token,
     tokenSecret,
     verifier
   }: ICallbackOptions): Promise<ICallbackResponse> {
-    return new Promise((resolve, reject) => {
-      if (!token && typeof token !== 'string') {
-        return reject(new Error('AsyncTwitterLogin: `token` is required'))
+    return await new Promise((resolve, reject) => {
+      if (typeof token !== 'string') {
+        reject(new Error('AsyncTwitterLogin: `token` is required'))
+        return
       }
-      if (!tokenSecret && typeof tokenSecret !== 'string') {
-        return reject(new Error('AsyncTwitterLogin: `tokenSecret` is required'))
+      if (typeof tokenSecret !== 'string') {
+        reject(new Error('AsyncTwitterLogin: `tokenSecret` is required'))
+        return
       }
-      if (!verifier && typeof verifier !== 'string') {
-        return reject(new Error('AsyncTwitterLogin: `verifier` is required'))
+      if (typeof verifier !== 'string') {
+        reject(new Error('AsyncTwitterLogin: `verifier` is required'))
+        return
       }
 
       this.oauth.getOAuthAccessToken(token, tokenSecret, verifier,
         (error, userToken, userTokenSecret, results) => {
-          if (error) return reject(error)
+          if (error != null) {
+            reject(error)
+            return
+          }
 
           const { user_id: id, screen_name: userName } = results
-          return resolve({
+          resolve({
             id,
             userName,
             token: userToken,
